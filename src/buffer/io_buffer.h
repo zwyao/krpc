5fde8f71c4cb1b9af9df6ff194e3f532da4b8ce7
zwyao <zwyyao@gmail.com>
2016-11-07 11:54:27 +0800

    尝试write影响吞吐量

--- src/buffer/io_buffer.h
+++ src/buffer/io_buffer.h
@@ -24,6 +24,7 @@ class IOBuffer
 
         int read(int fd, int max = -1);
         int write(int fd, bool& null_loop);
+        int write(int fd, BufferList::BufferEntry* entry);
 
         inline void release()
         {
